71559f4b3f406c98092627be1ceeec443211ce03
Subhrajit Swain <136870205+Subhrajit-14@users.noreply.github.com>
2024-10-14 23:56:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-
 let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = string.replace('x', '*');
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+let buttons = document.querySelectorAll(".button");
+const inputField = document.querySelector("input");
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    try {
+      if (e.target.innerHTML == "=") {
+        string = string.replace("x", "*");
+        let result = eval(string);
+        if (result === Infinity || isNaN(result)) {
+          throw new Error("Unsupported Operation");
+        }
+
+        string = result;
+        inputField.value = string;
+      } else if (e.target.innerHTML == "C") {
+        string = "";
+        inputField.value = string;
+      } else if (e.target.innerHTML == "DEL") {
+        string = string.substring(0, string.length - 1);
+        inputField.value = string;
+      } else {
+        string = string + e.target.innerHTML;
+        inputField.value = string;
       }
-  })
-})
+      inputField.scrollLeft = inputField.scrollWidth;
+
+    } catch (error) {
+      string = "Error";
+      inputField.value = error.message;
+    }
+  });
+});
